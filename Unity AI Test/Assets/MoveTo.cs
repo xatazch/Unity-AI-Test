8e2f7b457360bda46673d4070b41c199033ccd09
xatazch <pierre.fransson@outlook.com>
2017-11-19 21:42:42 +0100

    s

---
+++ Unity AI Test/Assets/MoveTo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTo : MonoBehaviour
+{
+    public float speed;
+    public Transform goal;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = goal.position - this.transform.position;
+
+        if (direction.magnitude > 1)
+            this.transform.Translate(direction.normalized * speed * Time.deltaTime);
+    }
+}
---
+++ Unity AI Test/Assets/PlayerControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public float maxSpeed;
+    Rigidbody playerRidgidbody;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        playerRidgidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        //playerRidgidbody.velocity = new Vector3(move * maxSpeed, playerRidgidbody.velocity.y, playerRidgidbody.velocity.x);
+    }
+}
